df5cb6ec28791af5f4ca8ddfab6e670fae231400
Jonathan Guiang <jkguiang@gmail.com>
2021-04-20 16:23:25 -0700

    fixed

--- client/run.sh
+++ client/run.sh
@@ -1,10 +1,11 @@
-delays_x5ms=$(seq 1 10)
+delays_x5ms=$(seq 0 10)
 
 mkdir -p outputs
 
 for delay_x5ms in ${delays_x5ms}; do
     delay_ms=$((${delay_x5ms}*5))
     echo "Running with a ${delay_ms}ms delay..."
+    output_json="test_${delay_ms}ms.json"
     # Add delay
     tc qdisc add dev eth0 root netem delay ${delay_ms}ms
     # Run test
