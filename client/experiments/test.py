73dec650d4bc5ccab292ba5e181ea1f4b282e149
Jonathan Guiang <jkguiang@gmail.com>
2021-04-20 11:52:17 -0700

    changed to uproot example for reading over xrootd

--- client/experiments/test.py
+++ client/experiments/test.py
@@ -1,12 +1,17 @@
-def read_byte_by_byte(input_binary, chunk_size=4):
-    with open(input_binary, "rb") as f_in:
-        # Read the first chunk of bytes
-        byte = f_in.read(chunk_size)
-        while byte != b"":
-            # Do stuff with this chunk of bytes
-            print(byte)
-            # Read the next chunk of bytes
-            byte = f_in.read(chunk_size)
+import uproot
+
+def test(file_name, redirector="172.17.0.2:1094", chunk_size=4):
+    # Define connection to XRootD file
+    xrd_path = f"root://{redirector}//{file_name}"
+    xrd_file = uproot.source.xrootd.XRootDResource(xrd_path, timeout=10)
+    # Read in chunk-by-chunk
+    n_bytes = xrd_file.num_bytes
+    byte_ranges = [(b, min(b+chunk_size, n_bytes)) 
+                   for b in range(0, n_bytes, chunk_size)]
+    for start, stop in byte_ranges:
+        print(xrd_file.get(start=start, stop=stop))
+
+    return
 
 if __name__ == "__main__":
-    read_byte_by_byte("inputs/test.dat", chunk_size=24)
+    test("test.dat")
