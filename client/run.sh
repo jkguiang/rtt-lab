06af3e178b93c8eb584c6c5f6599a7c0b7fedc2b
Jonathan Guiang <jkguiang@gmail.com>
2021-04-22 11:05:38 -0700

    configured to just run unit tests

--- client/run.sh
+++ client/run.sh
@@ -5,24 +5,24 @@ if [[ "${experiment}" != "" ]]; then
     delays_x5ms=$(seq 0 10)
     # Make the outputs directory if it doesn't exist already
     mkdir -p outputs
+    mkdir -p outputs/${experiment}
     # Run tests
     for delay_x5ms in ${delays_x5ms}; do
         delay_ms=$((${delay_x5ms}*5))
         echo "Running ${experiment}.py with a ${delay_ms}ms delay..."
-        output_json="${experiment}_${delay_ms}ms.json"
+        output_json="outputs/${experiment}/${experiment}_${delay_ms}ms.json"
         # Add delay
         tc qdisc add dev eth0 root netem delay ${delay_ms}ms
         # Run test
         python experiments/${experiment}.py \
-            --input_file=test.dat \
             --server="172.17.0.2:1094" \
-            --output_json="outputs/${output_json}" \
-            --chunk_size=4
+            --output_json=${output_json}
         # Remove delay
         tc qdisc del dev eth0 root netem delay ${delay_ms}ms
+        # Compress output
+        gzip ${output_json}
         echo "Done."
     done
-
 else
     echo "Usage: ./run.sh <name of experiment>"
 fi
