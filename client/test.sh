62d67983b8f6044e9e1e21a08f33eab845d54b37
Jonathan Guiang <jkguiang@gmail.com>
2021-04-22 11:13:30 -0700

    updated to reflect new simple unit test

--- client/test.sh
+++ client/test.sh
@@ -1,13 +1,6 @@
-delay_ms=${1}
-
 # Add delay
 tc qdisc add dev eth0 root netem delay ${1}ms
-
 # Run test
-python experiments/test.py \
-    --input_file=test.dat \
-    --server="172.17.0.2:1094" \
-    --chunk_size=4
-
+python experiments/simple_test.py --server="172.17.0.2:1094"
 # Remove delay
 tc qdisc del dev eth0 root netem delay ${1}ms
