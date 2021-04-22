ecc57f29b35b52c7b5b3f771ea7816100e97ad89
Jonathan Guiang <jkguiang@gmail.com>
2021-04-22 11:05:02 -0700

    added docs

--- client/experiments/utils.py
+++ client/experiments/utils.py
@@ -1,23 +1,11 @@
-import uproot
 import time
 
-class RTTSource(uproot.source.xrootd.XRootDSource):
-
-    def __init__(self, file_path, **options):
-        super().__init__(file_path, **options)
-        self.report = {"reads": [], "runtime":-999}
-
-    def chunk(self, start, stop):
-        d = dict(which="chunk", start=start, stop=stop, nbytes=stop-start)
-        self.report["reads"].append(d)
-        return super().chunk(start, stop)
-
-    def chunks(self, ranges, notifications):
-        d = dict(which="chunk", ranges=ranges, notifications=notifications)
-        self.report["reads"].append(d)
-        return super().chunks(ranges, notifications)
-
 def rtt_test(func):
+    """
+    Wrapper for an arbitrary RTT test function that adds the runtime to the report 
+    that it produces
+    (NOTE: RTT test function MUST return a dict!)
+    """
     def wrapper(*args, **kwargs):
         t0 = time.time()
         report = func(*args, **kwargs)
