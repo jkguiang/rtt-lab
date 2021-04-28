bba36ac6c1cac986be42dd75d81291700be59106
Jonathan Guiang <jkguiang@gmail.com>
2021-04-27 18:06:48 -0700

    now parses arguments and passes the rest to simple_test.py

--- client/test.sh
+++ client/test.sh
@@ -1,6 +1,15 @@
+# Parse arguments
+delay=0
+for arg in "$@"; do
+    key=$(echo $arg | cut -f1 -d=)
+    val=$(echo $arg | cut -f2 -d=)   
+    case "$key" in
+        --delay) delay=${val}; shift;;
+    esac    
+done
 # Add delay
-tc qdisc add dev eth0 root netem delay ${1}ms
+tc qdisc add dev eth0 root netem delay ${delay}ms
 # Run test
-python experiments/simple_test.py --server="172.17.0.2:1094"
+python experiments/simple_test.py ${@}
 # Remove delay
-tc qdisc del dev eth0 root netem delay ${1}ms
+tc qdisc del dev eth0 root netem delay ${delay}ms
